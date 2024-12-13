37bd818c05348a0c8a420a6fbf608d26b7b3efc9
mahmoud malek <information.mahmoud@gmail.com>
2024-12-13 23:35:58 +0200

    added for merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Results from "./pages/Results";
+import HowToUse from "./pages/HowToUse";
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
@@ -10,6 +13,10 @@ function App() {
 		  <Routes>
 			  <Route path='/' element={<Home />} />
 			  <Route path='/results' element={<Results />} />
+			  
+			  <Route path='/how-to-use' element={<HowToUse />} />
+			  <Route path='/login' element={<Login />} />
+			  <Route path='/signup' element={<SignUp />} />
 		  </Routes>
 	  </Router>
   );
